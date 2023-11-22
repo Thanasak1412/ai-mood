c93e2699502883e226aa410e5d1815e0215ceafc
Thanasak1412 <Thanasak.sr60@snru.ac.th>
2023-11-22 08:23:03 +0700

    Update sync user from clerk user

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,3 +1,22 @@
-export default function Page() {
-  return <>Hi</>;
+import { redirect } from "next/navigation";
+
+import { syncNewUser } from "@/utils/auth";
+import { currentUser } from "@clerk/nextjs";
+import { User as ClerkUser } from "@clerk/nextjs/dist/types/server";
+
+const createNewUser = async (clerkUser: ClerkUser | null) => {
+  if (!clerkUser) {
+    return;
+  }
+
+  await syncNewUser(clerkUser);
+
+  redirect('/journal');
+};
+
+export default async function Page() {
+  const clerkUser = await currentUser();
+  await createNewUser(clerkUser);
+
+  return <>...loading</>;
 }
