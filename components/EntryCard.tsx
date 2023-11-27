ea062d67b2af1379698d76f7833ff3d8ff7ca71a
Thanasak1412 <Thanasak.sr60@snru.ac.th>
2023-11-27 07:38:32 +0700

    feat: add qa to vectors & similarity searching with ai

--- components/EntryCard.tsx
+++ components/EntryCard.tsx
@@ -1,17 +1,25 @@
-import { JournalEntry } from "@prisma/client";
+import { Analysis, JournalEntry } from "@prisma/client";
 
 type Props = {
-  entry: JournalEntry;
+  entry: {
+    analysis: Pick<Analysis, 'summary' | 'mood'> | null;
+  } & JournalEntry;
 };
 
 export default function EntryCard({ entry }: Readonly<Props>) {
   const date = new Date(entry.createdAt).toDateString();
 
+  if (!entry.analysis) {
+    return;
+  }
+
+  const { summary, mood } = entry.analysis;
+
   return (
-    <ul className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow px-4">
+    <ul className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white px-4 shadow">
       <li className="py-5">{date}</li>
-      <li className="py-5">Summary</li>
-      <li className="py-4">Mood</li>
+      <li className="py-5">Summary: {summary}</li>
+      <li className="py-4">Mood: {mood}</li>
     </ul>
   );
 }
--- utils/api.ts
+++ utils/api.ts
@@ -34,3 +34,22 @@ export async function updateJournal(id: string, content: string) {
 
   return data;
 }
+
+export async function askQuestion(body: HTMLFormElement) {
+  const formData = new FormData(body);
+
+  const res = await fetch(
+    new Request(createUrl('/api/question'), {
+      method: 'POST',
+      body: formData,
+    }),
+  );
+
+  if (!res.ok) {
+    throw new Error('Error while to ask question');
+  }
+
+  const { data } = (await res.json()) as unknown as { data: string };
+
+  return data;
+}
